01fb710dde12b6f29ca741afbbecbb3ffe581842
huimei-tp <80399909+huimei-tp@users.noreply.github.com>
2025-02-03 12:15:44 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'), // Output - Dynamically generated filenames 
         filename: '[name].[contenthash].js',
-        // publicPath: '/healthcare-application-e-portfolio/', // Adding this make npx webpack serve not work...
+        publicPath: '/healthcare-application-e-portfolio/'
         // publicPath:'/',
         clean: true,
     },
